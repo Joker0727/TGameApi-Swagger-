36ebc4675b8e559c2207a2f8343615fe63baf456
Joker0727 <1610779207@qq.com>
2018-08-17 15:40:40 +0800

    重新提交遗漏的实体类

---
+++ WebSwaggerApi/Models/TGameModel/GardenSportAndSleepDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSwaggerApi.Models.TGameModel
+{
+    public class GardenSportAndSleepDto
+    {
+        public List<GardenSleep> gardenSleep { get; set; }
+        public List<GardenSport> gardenSport { get; set; }
+    }
+
+    public class GardenSleep
+    {
+        public int? GradeNum { get; set; }
+        public double AverageSleepTime { get; set; }
+    }
+    public class GardenSport
+    {
+        public int? GradeNum { get; set; }
+        public double AverageSport { get; set; }
+    }
+}
---
+++ WebSwaggerApi/Models/TGameModel/SingleCitySportDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSwaggerApi.Models.TGameModel
+{
+    public class SingleCitySportDto
+    {
+        public string Date { get; set; }
+
+        public int HeavyCount { get; set; }
+        public int CenterCount { get; set; }
+        public int LowerCount { get; set; }
+    }
+}
